b566fc6f2b2547b2b591ae97cd4d780e8208e544
coolkat2035 <randomkat2035@gmail.com>
2021-08-31 13:24:01 +0800

    minor issues

--- asciiConverter/convert.py
+++ asciiConverter/convert.py
@@ -1,36 +1,50 @@
 import math, sys, os, time
 try:
-    from PIL import Image
+    from PIL import Image, ImageEnhance
 except ModuleNotFoundError:
     os.system("pip install Pillow")
 
 
 
-MAXW = 150
+MAXW = 150#default
 args = sys.argv
 lum_map = "@#$%&?/:,."#lightmode
 img_name = "defaultNameYouShouldntSeeThis.png"#image name lmao should be updated
 name_noext = "defaultNameYouShouldntSeeThis"
 
-try:#if user typing in cmd or clicking on the file (no argvs)
-    img_name = args[1]#cmd
-    if "-d" in args:
-        lum_map = lum_map[::-1]#switch to darkmode
+try:# clicking ot cmd?
+    img_name = args[1]# cmd
+
+    if "-d" in args[2:]:
+        lum_map = lum_map[::-1]# switch to darkmode
+
+    if "--max-width" in args[2:]:
+        MAXW = args.index(args.index("--max-width") +1)#the argument after this option
+
+    
 
 except IndexError:#clicking
-    img_name = input("Enter your input image path here (with extension!!!)> ")
+    img_name = input("Enter your input image path here (with extension!!!)>> ")
 
     while True:
-        is_flip = input("Do you want to flip the brightness? Default is light background dark text.(y/n)")
+        is_flip = input("Do you want to invert the brightness? Default is light background dark text. (y/n)>> ")
         if is_flip.startswith('y'):
-            print("Alright, moving on.")
+            print("Inverted!")
             lum_map = lum_map[::-1]#switch to dark mode
             break
         elif is_flip in ['n', "no", "nah", "nope"]:
-            print("Got it, moving on")
             break
         else:
             print("I don't really understand...\n")#bruh stop playing its not funny
+            continue
+
+    while True:
+        try:
+            MAXW = int(input("What's the max width?>> "))
+            print("Moving on.")
+            break
+        except:
+            print("That is not an integer!")
             continue
 
 ############################Stuff gets real now####################################
@@ -55,9 +69,13 @@ def convertToString(target:str):
             img = img.resize((MAXW,hsize))
 
             img = img.convert("L")
+            
+
+            #ImageEnhance.Sharpness(img).enhance(2.0)#doesn't seem to work
 
             #img.show()#DEBUG
 
+            print("Generating...Shouldn't take too long.")
             for y in range(img.height):#row
                 for x in range(img.width):#column
                     lum = img.getpixel((x,y))
